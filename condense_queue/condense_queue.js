6f2530bea35ea2b05856907dc4b1d641c2abf8cb
kesre <david.ersek@twonil.com>
2016-08-10 05:21:10 -0700

    V0.3 Clean up code; JSLint

--- condense_queue/condense_queue.js
+++ condense_queue/condense_queue.js
@@ -1,48 +1,100 @@
-var queue_items = document.getElementsByClassName("queue-item");
-var up_to_date = [];
+/*jslint browser:true */
+var QUEUE_ITEM_CLASS = "queue-item";
+var PROGRESS_BAR_CLASS = "episode-progress";
+var SIDEBAR_ID = "sidebar_elements";
+var TITLE_TOGGLE_COLOR = "gold";
 
-for (i=0; i < queue_items.length; i++) {
-    queue_item = queue_items[i];
-    var progress = queue_item.getElementsByClassName("episode-progress")[0];
-    if (progress.style.width.length > 2 & progress.style.width[0] > "7") {
-        queue_item.style.display = "none";
-        var index = up_to_date.push(queue_item);
-        queue_item.id = "up_to_date_" + (index - 1)
-    }
+function hideWatchedQueueItems() {
+    "use strict";
+    // Find and hide queue elements. Progress bar width (hackily) allows
+    // checking whether the latest episode was finished.
+
+    var up_to_date = [];
+    var progress;
+    var index;
+    [].forEach.call(
+        document.getElementsByClassName(QUEUE_ITEM_CLASS),
+        function (queue_item) {
+            progress = queue_item.getElementsByClassName(
+                PROGRESS_BAR_CLASS
+            )[0];
+            if (
+                progress.style.width.length > 2 &&
+                progress.style.width[0] > "7"
+            ) {
+                queue_item.style.display = "none";
+                index = up_to_date.push(queue_item);
+                queue_item.id = "up_to_date_" + (index - 1);
+            }
+        }
+    );
+    return up_to_date;
 }
 
-
-var titles = document.createElement('li');
-var titles_content = document.createElement('ul');
-var titles_desc = document.createElement('h3');
-titles_desc.appendChild(document.createTextNode("Up to date"));
-titles.appendChild(titles_desc);
-titles.appendChild(titles_content);
 
 function toggleVisibility(title_id) {
+    "use strict";
     var queue_item = document.getElementById(title_id);
-    queue_item.style.display = queue_item.style.display == 'block'? 'none' : 'block';
+    queue_item.style.display = (
+        queue_item.style.display === "block"
+            ? "none"
+            : "block"
+    );
 }
 
+
 function titleToggler(title_id) {
-    return function(evt) {
-        toggleVisibility(title_id)
-        evt.currentTarget.style.color = evt.currentTarget.style.color == 'gold'? '': 'gold';
+    "use strict";
+    return function (evt) {
+        toggleVisibility(title_id);
+        evt.currentTarget.style.color = (
+            evt.currentTarget.style.color === TITLE_TOGGLE_COLOR
+                ? ""
+                : TITLE_TOGGLE_COLOR
+        );
     };
 }
 
-for (i=0; i < up_to_date.length; i++) {
-    title_text = up_to_date[i].getElementsByClassName(
-        'series-title ellipsis'
-    )[0].textContent;
-    var title = document.createElement('li');
-    title.style.cursor = 'pointer';
-    title.className = "series-title block ellipsis";
-    title.appendChild(document.createTextNode(title_text));
-    var title_id = 'up_to_date_' + i;
-    title.onclick = titleToggler(title_id);
-    titles_content.appendChild(title);
-}
-var sidebar_list = document.getElementById("sidebar_elements");
-sidebar_list.appendChild(titles);
 
+function createSidebarTitles(up_to_date) {
+    "use strict";
+    // Add a way to toggle visiblity (for sorting or if the episode wasn't
+    // actually finished)
+    var titles = document.createElement("li");
+    var titles_content = document.createElement("ul");
+    var titles_desc = document.createElement("h3");
+    titles_desc.appendChild(document.createTextNode("Up to date"));
+    titles.appendChild(titles_desc);
+    titles.appendChild(titles_content);
+
+    var title_text;
+    var title;
+    var title_id;
+    up_to_date.forEach(function (show_element, i) {
+        title_text = show_element.getElementsByClassName(
+            "series-title ellipsis"
+        )[0].textContent;
+        title = document.createElement("li");
+        title.style.cursor = "pointer";
+        title.className = "series-title block ellipsis";
+        title.appendChild(document.createTextNode(title_text));
+        title_id = "up_to_date_" + i;
+        title.onclick = titleToggler(title_id);
+        titles_content.appendChild(title);
+    });
+
+    return titles;
+}
+
+
+function main() {
+    "use strict";
+    var sidebar_list = document.getElementById(SIDEBAR_ID);
+    var up_to_date_container = hideWatchedQueueItems();
+    sidebar_list.appendChild(
+        createSidebarTitles(up_to_date_container)
+    );
+}
+
+
+main();
